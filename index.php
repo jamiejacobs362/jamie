fba9e8af51c9ab898755499b314d03b833f97a61
jamiejacobs362 <125606556+jamiejacobs362@users.noreply.github.com>
2023-04-05 11:10:25 +1200

    change website structure

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+<title>Page Title</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="css/style.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+<div class="header">
+  <h1>My Website</h1>
+  <p>A website created by me.</p>
+</div>
+
+<div class="navbar">
+  <a href="#">Link</a>
+  <a href="#">Link</a>
+  <a href="#">Link</a>
+  <a href="#" class="right">Link</a>
+</div>
+<div class="row">
+  <div class="side">side</div>
+  <div class="main">main</div>
+</div>
+
+<div class="footer">
+  <h2>Footer</h2>
+</div>
+
+</body>
+
+</html>
