6d5e28a99700fc2c4063c047681a116dec992c3c
jamiejacobs362 <125606556+jamiejacobs362@users.noreply.github.com>
2023-05-10 10:18:20 +1200

    added cart php page

---
+++ cart.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+<title>Kura Canteen-suggestions Page</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="css/style.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+<div class="header">
+  <img src="images/heading.jpg" alt="">
+  <p>This is the sug</p>
+</div>
+
+<?php
+include "navbar.php";
+?>
+<div class="row">
+  <div class="side"><img src="images/beef.jpg" alt=""></div>
+  <div class="main">main</div>
+</div>
+
+<div class="footer">
+  <h2>Footer</h2>
+</div>
+
+</body>
+
+</html>
