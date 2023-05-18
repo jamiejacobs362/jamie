caadab49fc69115eb64ebe94bc7ce165ebaf586a
jamiejacobs362 <125606556+jamiejacobs362@users.noreply.github.com>
2023-05-19 10:25:53 +1200

    changed colour of website, added a wrapper and images
    changed colour backgrounds of website and tried to make galley of my images

--- index.php
+++ index.php
@@ -7,6 +7,29 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link href="css/style.css" rel="stylesheet" type="text/css">
+<style>
+div.gallery {
+  margin: 5px;
+  border: 1px solid #ccc;
+  float: left;
+  width: 180px;
+}
+
+div.gallery:hover {
+  border: 1px solid #777;
+}
+
+div.gallery img {
+  width: 100%;
+  height: auto;
+}
+
+div.desc {
+  padding: 15px;
+  text-align: center;
+}
+</style>
+
 </head>
 
 <body>
@@ -19,9 +42,24 @@
 include "navbar.php";
 ?>
 <div class="row">
-  <div class="side"><img src="images/beef.jpg" alt=""></div>
-  <div class="main">main</div>
+  <div class="side"><img src="images/shelf1.jpg" alt="Shelf where food avabile is shown" width="600" height="400"></div>
+  <div class="main">
+    <div class="gallery">
+  <a target="_blank" href="images/shelf2.jpg">
+    <img src="images/shelf2.jpg" alt="beef" width="600" height="400">
+  </a>
 </div>
+</div>
+</div>
+
+
+<div class="gallery">
+  <a target="_blank" href="images/shelf3.jpg">
+    <img src="images/shelf3.jpg" alt="Shelf where food avabile is shown" width="600" height="400">
+  </a>
+
+</div>
+
 
 <div class="footer">
   <h2>Footer</h2>
--- order.php
+++ order.php
@@ -3,10 +3,33 @@
 <html lang="en">
 
 <head>
-<title>Kura Canteen-Home Page</title>
+<title>order</title>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link href="css/style.css" rel="stylesheet" type="text/css">
+
+<style>
+div.gallery {
+  margin: 5px;
+  border: 1px solid #ccc;
+  float: left;
+  width: 180px;
+}
+
+div.gallery:hover {
+  border: 1px solid #777;
+}
+
+div.gallery img {
+  width: 100%;
+  height: auto;
+}
+
+div.desc {
+  padding: 15px;
+  text-align: center;
+}
+</style>
 </head>
 
 <body>
@@ -20,7 +43,38 @@ include "navbar.php";
 ?>
 <div class="row">
   <div class="side"><img src="images/beef.jpg" alt=""></div>
-  <div class="main">main</div>
+  <div class="main">
+    <div class="gallery">
+  <a target="_blank" href="images/beef.jpg">
+    <img src="images/beef.jpg" alt="beef" width="600" height="400">
+  </a>
+  <div class="desc">this is our beef slice</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="images/bread.jpg">
+    <img src="images/bread.jpg" alt="bread" width="600" height="400">
+  </a>
+  <div class="desc">this is our bread slices</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="images/churros.jpg">
+    <img src="images/churros.jpg" alt="churros" width="600" height="400">
+  </a>
+  <div class="desc">this is our beef slice</div>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="images/beef.jpg">
+    <img src="images/beef.jpg" alt="beef" width="600" height="400">
+  </a>
+  <div class="desc">this is our beef slice</div>
+
+</div>
+
+</div>
+
 </div>
 
 <div class="footer">
