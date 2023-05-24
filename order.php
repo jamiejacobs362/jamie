4b3f8a130abe363c4b02f8c97b2ee4c1a19d2186
jamiejacobs362 <jamieja@whangamata.school.nz>
2023-05-24 12:49:33 +1200

    adding images to order page
    adding images to order page and trying to make some images go under each other

--- order.php
+++ order.php
@@ -42,41 +42,51 @@ div.desc {
 include "navbar.php";
 ?>
 <div class="row">
-  <div class="side"><img src="images/beef.jpg" alt=""></div>
   <div class="main">
     <div class="gallery">
-  <a target="_blank" href="images/beef.jpg">
-    <img src="images/beef.jpg" alt="beef" width="600" height="400">
+  <a target="_blank" href="images/mac&cheese.jpg">
+    <img src="images/mac&cheese.jpg" alt="mac&cheese" width="100%">
   </a>
-  <div class="desc">this is our beef slice</div>
+  <div class="desc">This our juicie</div>
+        <div class="gallery">
+  <a target="_blank" href="images/juicie.jpg" width="100%">
+    <img src="images/juicie.jpg" alt="juicie" width="100%">
+  </a>
+  <div class="desc">This our water</div>
 </div>
-
+</div>
 <div class="gallery">
-  <a target="_blank" href="images/bread.jpg">
-    <img src="images/bread.jpg" alt="bread" width="600" height="400">
+  <a target="_blank" href="images/water.jpg">
+    <img src="images/water.jpg" alt="water" width="100%">
   </a>
-  <div class="desc">this is our bread slices</div>
-</div>
-
-<div class="gallery">
-  <a target="_blank" href="images/churros.jpg">
-    <img src="images/churros.jpg" alt="churros" width="600" height="400">
+  <div class="desc">This our water</div>
+    
+     <div class="gallery">
+  <a target="_blank" href="images/juicie2.jpg">
+    <img src="images/juicie2.jpg" alt="juicie" width="100%">
   </a>
-  <div class="desc">this is our beef slice</div>
+  <div class="desc">This our juicie</div>
 </div>
-
-<div class="gallery">
-  <a target="_blank" href="images/beef.jpg">
-    <img src="images/beef.jpg" alt="beef" width="600" height="400">
+</div>
+      <div class="gallery">
+  <a target="_blank" href="images/hotdog.jpg">
+    <img src="images/hotdog.jpg" alt="hotdog" width="100%">
   </a>
-  <div class="desc">this is our beef slice</div>
-
+  <div class="desc">This our hotdog</div>
+</div> 
+   <div class="gallery">
+  <a target="_blank" href="images/hotdog.jpg">
+    <img src="images/IMG_8479.jpg" alt="hotdog" width="100%">
+  </a>
+  <div class="desc">This our hotdog</div>
+</div> 
+      
 </div>
-
 </div>
+    
 
-</div>
-
+    
+    
 <div class="footer">
   <h2>Footer</h2>
 </div>
